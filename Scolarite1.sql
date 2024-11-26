0ad57c2295d67074172f5fedd2db60094369cfbb
Zeinot <zeinotgaming@gmail.com>
2024-11-26 10:27:46 +0100

    s

---
+++ Scolarite1.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Etudiant (
+    IdEt VARCHAR2(10) PRIMARY KEY,
+    NomEt VARCHAR2(255),
+    PrenomEt VARCHAR2(255),
+    DateNais DATE
+);
+CREATE SEQUENCE etudiant_seq
+  MINVALUE 1
+  MAXVALUE 999999999999999999999999999
+  START WITH 1
+  INCREMENT BY 1
+  CACHE 20;
+
+CREATE OR REPLACE TRIGGER trg_etudiant_id
+BEFORE INSERT ON Etudiant
+FOR EACH ROW
+BEGIN
+  :NEW.IdEt := 'ET' || LPAD(etudiant_seq.NEXTVAL, 4, '0');
+END;
+/
+
+
+
+
+
+-- Insert data into the Etudiant table
+INSERT INTO Etudiant (NomEt, PrenomEt, DateNais) 
+    VALUES ('Dupont', 'Jean', TO_DATE('1995-04-15', 'YYYY-MM-DD'));
+INSERT INTO Etudiant (NomEt, PrenomEt, DateNais) 
+    VALUES ('Martin', 'Alice', TO_DATE('1997-08-21', 'YYYY-MM-DD'));
+INSERT INTO Etudiant (NomEt, PrenomEt, DateNais) 
+    VALUES ('Durand', 'Sophie', TO_DATE('1999-12-30', 'YYYY-MM-DD'));
+
+-- Check the inserted data
+SELECT * FROM Etudiant;
+
---
+++ main.py
@@ -0,0 +1,45 @@
+import oracledb
+user = "directeur"
+pwd = "admin"
+connection = oracledb.connect(
+    user=user,
+    password=pwd,
+    host="localhost", 
+    port=1521, 
+    service_name="scolarite")
+cursor = connection.cursor()
+print("version : ", connection.version)
+
+
+
+for row in cursor.execute("SELECT * FROM Etudiant"):
+    print(row)
+    
+    
+cursor.execute("SELECT * FROM Etudiant")
+while True:
+    row = cursor.fetchone()
+    if row is None:
+        break
+    print(row)
+    
+    
+cursor.execute("SELECT IDET from Etudiant")
+rows = cursor.fetchall()
+print(rows)
+print(f"Number of rows: {len(rows)}")
+if rows:
+    for row in rows:
+        print(row)
+else:
+    print("No data found.")
+    
+    
+# Execute a simple query to test the connection 
+cursor.execute("SELECT SYSDATE FROM dual") 
+result = cursor.fetchone() 
+# Print the result 
+print("Current database date and time:", result[0])
+
+cursor.close() 
+connection.close()
